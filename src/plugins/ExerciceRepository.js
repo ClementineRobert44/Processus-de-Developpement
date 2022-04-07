33c88e298b79df43d8da2fd7228bb960a86e209c
Vanane <blackfox248@gmail.com>
2022-04-07 18:09:14 +0200

    Ajout update etat exercice

--- src/plugins/ExerciceRepository.js
+++ src/plugins/ExerciceRepository.js
@@ -7,12 +7,14 @@ function init(plugin, options) {
 export default {
 	install: (app, options) => {
 		app.config.globalProperties.$exerciceRepository = {
-			/** Retourne un exercice par son id */
+            baseURL: options.baseURL,
+
+			/** Retourne un Object représentant un exercice par son id */
 			async getExercice(idExercice) {	
                 var exerciceSheet = await app.config.globalProperties.$sheetsApi.getSheetWithName("Exercices"); // Dans le template, la feuille 5 est la feuille "Exercices"
                 
                 var fieldsRow = exerciceSheet.data[0].rowData[0];
-                var exerciceRow = exerciceSheet.data[0].rowData.find(e => e.values[0].formattedValue == idExercice);
+                var exerciceRow = this.exerciceExists(idExercice);
 
                 if(exerciceRow) {
                     return this.createFromRow(exerciceRow, fieldsRow);
@@ -46,6 +48,40 @@ export default {
                 return ret;
             },
 
+
+            /**
+             * Retourne un objet de type SheetRow si un exercice correspond à l'id passé.
+             * Retourne NULL sinon.
+             */
+            async exerciceExists(idExercice) {
+                var exerciceSheet = await app.config.globalProperties.$sheetsApi.getSheetWithName("Exercices");
+                
+                return exerciceSheet.data[0].rowData.find(e => e.values[0].formattedValue == idExercice);
+            },
+
+
+            /* Change l'état d'un exercice */
+            async updateEtat(idExercice, newEtat) {
+                var sheetName = 'Exercices';
+                var exerciceSheet = await app.config.globalProperties.$sheetsApi.getSheetWithName("Exercices");
+                var cellCol = app.config.globalProperties.$sheetsStructure.structure.sheets.exercices.col.Etat;
+                var cellRow = null;                
+                var index = 1;
+
+                while(!cellRow && exerciceSheet.data[0].rowData.length > index) {
+                    var row = exerciceSheet.data[0].rowData[index];
+
+                    if(row.values[0].formattedValue == idExercice)
+                        cellRow = index + 1; // + 1 car les exercices commencent à la ligne 2 dans le Sheets
+                    else
+                        index++;
+                }
+
+                if(!cellRow) return;
+
+                var cellCoordinates = `${sheetName}!${cellCol}${cellRow}`;
+                await app.config.globalProperties.$sheetsApi.updateCell(cellCoordinates, newEtat);
+            },
 
             /** Retourne un objet JSON à partir d'une row de sheet Google
              * @param row row de la feuille de calcul à passer pour créer un objet
