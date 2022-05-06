ca353cce2f492acc615dc45186264882204c5350
Vanane <blackfox248@gmail.com>
2022-05-06 15:17:47 +0200

    Fix : Afficher le numér ode l'étape plutôt que le numéro de l'exercice dans la liste d'un module

--- src/components/Program/ExerciceCard.vue
+++ src/components/Program/ExerciceCard.vue
@@ -1,69 +1,69 @@
 <template>
-  <router-link
-    :to="{
-      name: 'Exercice',
-      params: {
-        idE: exercice.Id
-      },
-    }"
-  >
-    <div class="card">
-      <div class="card-content">
-        <div class="media">
-          <div class="media-content">
-            <p class="title is-4">
-              <span class="numberExercice">{{ this.exercice.Id }}.</span>
-              {{ this.exercice.Titre }}
-            </p>
-          </div>
-        </div>
-        <div class="content">
-          {{ this.exercice.Description }}
-        </div>
-      </div>
-      <div
-        :class="{
-          'avancement isNotStart': this.exercice.Etat == '0',
-          'avancement isInProgress': this.exercice.Etat == '1',
-          'avancement isFinish': this.exercice.Etat == '2',
+    <router-link
+        :to="{
+            name: 'Exercice',
+            params: {
+                idE: exercice.Id,
+            },
         }"
-      >
-      </div>
-    </div>
-  </router-link>
+    >
+        <div class="card">
+            <div class="card-content">
+                <div class="media">
+                    <div class="media-content">
+                        <p class="title is-4">
+                            <span class="numberExercice">{{ this.idStep }}.</span>
+                            {{ this.exercice.Titre }}
+                        </p>
+                    </div>
+                </div>
+                <div class="content">
+                    {{ this.exercice.Description }}
+                </div>
+            </div>
+            <div
+                :class="{
+                    'avancement isNotStart': this.exercice.Etat == '0',
+                    'avancement isInProgress': this.exercice.Etat == '1',
+                    'avancement isFinish': this.exercice.Etat == '2',
+                }"
+            ></div>
+        </div>
+    </router-link>
 </template>
 
 <script>
 export default {
-  props: {
-    exercice: [],
-  },
+    props: {
+        idStep: Number,
+        exercice: [],
+    },
 };
 </script>
 
 <style>
 .numberExercice {
-  color: #f07969;
+    color: #f07969;
 }
 
 .avancement {
-  border-bottom-right-radius: 4px;
-  border-bottom-left-radius: 4px;
-  height:1em;
+    border-bottom-right-radius: 4px;
+    border-bottom-left-radius: 4px;
+    height: 1em;
 }
 
 .isNotStart {
-  background-color: #d05252;
-  color: #d05252;
+    background-color: #d05252;
+    color: #d05252;
 }
 
 .isInProgress {
-  background-color: #e1a751;
-  color: #e1a751;
+    background-color: #e1a751;
+    color: #e1a751;
 }
 
 .isFinish {
-  background-color: #63d052;
-  color: #63d052;
+    background-color: #63d052;
+    color: #63d052;
 }
 </style>
--- src/components/Program/ModuleDetailCard.vue
+++ src/components/Program/ModuleDetailCard.vue
@@ -1,44 +1,45 @@
 <template>
-  <div v-if="this.module && this.exercices">
-    <h1 class="title is-1">{{ this.module.Titre }}</h1>
-    <div class="columns is-multiline">
-      <ExerciceCard
-        class="column is-full-mobile is-half-tablet is-half-desktop is-half-widescreen is-half-fullhd"
-        v-for="item in this.exercices"
-        :key="item.Id"
-        :exercice="item"
-      />
+    <div v-if="this.module && this.exercices">
+        <h1 class="title is-1">{{ this.module.Titre }}</h1>
+        <div class="columns is-multiline">
+            <ExerciceCard
+                class="column is-full-mobile is-half-tablet is-half-desktop is-half-widescreen is-half-fullhd"
+                v-for="item in this.exercices"
+                :key="item.Id"
+                :idStep="this.exercices.findIndex((e) => e.Id == item.Id) + 1"
+                :exercice="item"
+            />
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 import ExerciceCard from "./ExerciceCard.vue";
 export default {
-  components: {
-    ExerciceCard,
-  },
-  props: {
-    idModule: String    
-  },
+    components: {
+        ExerciceCard,
+    },
+    props: {
+        idModule: String,
+    },
 
-  data() {
-    return {
-        module: {},
-        exercices: [],
-    };
-  },
+    data() {
+        return {
+            module: {},
+            exercices: [],
+        };
+    },
 
-  async mounted() {
-      this.module = await this.$moduleRepository.getModule(this.idModule);
-      this.exercices = await this.$exerciceRepository.getExercicesModule(this.module.Id);
-      console.log(this.exercices);
-  },
+    async mounted() {
+        this.module = await this.$moduleRepository.getModule(this.idModule);
+        this.exercices = await this.$exerciceRepository.getExercicesModule(this.module.Id);
+        console.log(this.exercices);
+    },
 };
 </script>
 
 <style>
 .columns {
-  margin: 10px;
+    margin: 10px;
 }
 </style>
